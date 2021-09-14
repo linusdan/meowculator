cdabb0047a4c91ee70bc7fb7a27b1d30b0cec4c1
Danilo Santos <artededan@protonmail.ch>
2021-09-14 16:01:47 -0300

    (meow) Adicionar e importar componente calculadora

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CalculadoraModule } from './modules/calculadora/calculadora.module';
+import { CalculadoraComponent } from './components/calculadora/calculadora.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalculadoraComponent
   ],
   imports: [
     BrowserModule,
