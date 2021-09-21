491b338722d526d035d5b5442ccedd2a696c237f
Danilo Santos <artededan@protonmail.ch>
2021-09-21 10:25:08 -0300

    (meow) Implementar o serviço de execução de operações da calculadora

--- src/app/services/calculadora.service.ts
+++ src/app/services/calculadora.service.ts
@@ -1,9 +1,48 @@
+/**
+* Serviço responsável por executar as operações da calculadora.
+* 
+* @author Danilo Santos
+* @version meow
+**/
+
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CalculadoraService {
+  
+  /* definição de constantes para identificar as operações de cálculo */
+  static readonly SOMAR: string = '+';
+  static readonly SUBTRAIR: string = '-';
+  static readonly DIVIDIR: string = '/';
+  static readonly MULTIPLICAR: string = '*';
 
   constructor() { }
+
+  /* método para efetuar as operações da calculadora */
+  calcular(num1: number, num2: number, operacao: string): number {
+
+    // armazena o resultado da operação
+    let resultado: number;
+
+    switch(operacao) {
+      case CalculadoraService.SOMAR:
+        resultado = num1 + num2;
+      break;
+      case CalculadoraService.SUBTRAIR:
+        resultado = num1 - num2;
+      break;
+      case CalculadoraService.DIVIDIR:
+        resultado = num1 / num2;
+      break;
+      case CalculadoraService.MULTIPLICAR:
+        resultado = num1 * num2;
+      break;
+      default:
+        resultado = 0;
+    }
+
+    return resultado;
+  }
 }
