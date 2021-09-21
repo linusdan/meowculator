cc7c26f9fd62eadc7f9b058234aa89710c70674a
Danilo Santos <artededan@protonmail.ch>
2021-09-21 12:07:38 -0300

    (meow) Jasmine: Testes de serviço e componentes

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,11 +2,14 @@ import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
+import { CalculadoraModule } from '../app/modules/calculadora/calculadora.module';
+
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        CalculadoraModule
       ],
       declarations: [
         AppComponent
--- src/app/services/calculadora.service.spec.ts
+++ src/app/services/calculadora.service.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed } from '@angular/core/testing';
+import { inject, TestBed } from '@angular/core/testing';
 
 import { CalculadoraService } from './calculadora.service';
 
@@ -12,5 +12,40 @@ describe('CalculadoraService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('Teste de soma: deve garantir que 2 + 2 = 4', () => {
+    inject([CalculadoraService], (service: CalculadoraService) => {
+      let somar = service.calcular(2, 2, CalculadoraService.SOMAR);
+      expect(somar).toEqual(4);
+    })
+  });
+
+  it('Teste de soma: deve garantir que 2 - 2 = 0', () => {
+    inject([CalculadoraService], (service: CalculadoraService) => {
+      let subtrair = service.calcular(2, 2, CalculadoraService.SUBTRAIR);
+      expect(subtrair).toEqual(0);
+    })
+  });
+
+  it('Teste de soma: deve garantir que 2 / 2 = 1', () => {
+    inject([CalculadoraService], (service: CalculadoraService) => {
+      let dividir = service.calcular(2, 2, CalculadoraService.DIVIDIR);
+      expect(dividir).toEqual(1);
+    })
+  });
+
+  it('Teste de soma: deve garantir que 2 * 2 = 4', () => {
+    inject([CalculadoraService], (service: CalculadoraService) => {
+      let multiplicar = service.calcular(2, 2, CalculadoraService.MULTIPLICAR);
+      expect(multiplicar).toEqual(4);
+    })
+  });
+
+  it('Deve retornar 0 para operação inválida', () => {
+    inject([CalculadoraService], (service: CalculadoraService) => {
+      let operacaoInvalida = service.calcular(1, 3, '%');
+      expect(operacaoInvalida).toEqual(0);
+    })
   });
 });
